6f44997d24a4d4d562118e611734579fe70d8c4d
diego7785 <diego.viafara@correounivalle.edu.co>
2020-03-14 13:36:33 -0500

    First commit project

---
+++ mande/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './assets/images/logo.png';
+import './assets/css/App.css';
+
+//ESTE DOCUMENTO TAMBIÉN DEBE MODIFICARSE 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          MANDE
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
