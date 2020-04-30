19b7adafe3f9a482be22bfd16cd9ca3d0043ef77
diego7785 <diego.viafara@correounivalle.edu.co>
2020-04-30 14:02:37 -0500

    corriendo error con las apikey

--- mande/src/components/Maps/map.js
+++ mande/src/components/Maps/map.js
@@ -9,7 +9,7 @@ import Geocode from "react-geocode";
 const AnyReactComponent = ({ text }) => <div><img src={require("assets/img/icons/common/dropPin.png")} alt={text} width="50"/></div>;
 
 
-Geocode.setApiKey('AIzaSyA3QdJU4tDb58Vvx46uIDEsJR-vcW8tF0w');
+Geocode.setApiKey('GEOCODING KEY');
 Geocode.setLanguage("es");
 Geocode.setRegion("co");
 Geocode.enableDebug();
@@ -29,7 +29,7 @@ class SimpleMap extends Component {
     return (
       <div style={{ height: '50vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyA3QdJU4tDb58Vvx46uIDEsJR-vcW8tF0w' }}
+          bootstrapURLKeys={{ key: 'GOOGLE MAPS KEY' }}
           defaultCenter={{lat: this.props.state.latitude, lng: this.props.state.length}}
           defaultZoom={15}
         >
