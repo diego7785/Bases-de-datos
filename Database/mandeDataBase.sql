aaadae3157fd6055b71c5d0240bc56ef8f9f8125
diego7785 <diego.viafara@correounivalle.edu.co>
2020-05-15 02:34:38 -0500

    encrypting info

--- Database/mandeDataBase.sql
+++ Database/mandeDataBase.sql
@@ -10,8 +10,19 @@ DROP TABLE IF EXISTS Servicio CASCADE;
 DROP TABLE IF EXISTS Paga CASCADE;
 DROP TABLE IF EXISTS Servicio_pago CASCADE;
 CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 DROP FUNCTION IF EXISTS add_geopint;
 DROP TRIGGER IF EXISTS trigger_add_geopint ON Direccion;
+DROP FUNCTION IF EXISTS encrypt_Wpass;
+DROP TRIGGER IF EXISTS trigger_encrypt_Wpass ON Trabajador;
+DROP FUNCTION IF EXISTS encrypt_Upass;
+DROP TRIGGER IF EXISTS trigger_encrypt_Upass ON Usuario;
+DROP FUNCTION IF EXISTS encrypt_bankAccount;
+DROP TRIGGER IF EXISTS trigger_encrypt_bankAccount ON Cuenta_bancaria;
+DROP FUNCTION IF EXISTS encrypt_Tdebit;
+DROP TRIGGER IF EXISTS trigger_encrypt_Tdebit ON Tarjeta_debito;
+DROP FUNCTION IF EXISTS encrypt_Tcredit;
+DROP TRIGGER IF EXISTS trigger_encrypt_Tcredit ON Tarjeta_credito;
 
 CREATE TABLE Trabajador(
 	cedula_trabajador VARCHAR(10) NOT NULL,
@@ -19,7 +30,7 @@ CREATE TABLE Trabajador(
 	trabajador_email VARCHAR(50) NOT NULL,
 	trabajador_nombre VARCHAR(70) NOT NULL,
 	trabajador_apellido VARCHAR(70) NOT NULL,
-	trabajador_contrasenia VARCHAR(50) NOT NULL,
+	trabajador_contrasenia VARCHAR(255) NOT NULL,
 	trabajador_foto_perfil VARCHAR(25),
 	trabajador_foto_id_frente VARCHAR(25) NOT NULL,
 	trabajador_foto_id_atras VARCHAR(25) NOT NULL,
@@ -32,7 +43,7 @@ CREATE TABLE Trabajador(
 	 usuario_email VARCHAR(50) NOT NULL,
 	 usuario_nombre VARCHAR(70) NOT NULL,
 	 usuario_apellido VARCHAR(70) NOT NULL,
-	 usuario_contrasenia VARCHAR(50) NOT NULL,
+	 usuario_contrasenia VARCHAR(255) NOT NULL,
 	 usuario_foto_perfil VARCHAR(25),
 	 usuario_foto_id_frente VARCHAR(25) NOT NULL,
 	 usuario_foto_id_atras VARCHAR(25) NOT NULL,
@@ -41,7 +52,7 @@ CREATE TABLE Trabajador(
  );
 
  CREATE TABLE Cuenta_bancaria(
- 	 numero_cuenta_bancaria VARCHAR(20) NOT NULL,
+ 	 numero_cuenta_bancaria VARCHAR(255) NOT NULL,
 	 cuenta_bancaria_banco VARCHAR(50) NOT NULL,
 	 cuenta_bancaria_tipo VARCHAR(30) NOT NULL,
 	 cedula_trabajador VARCHAR(10) NOT NULL,
@@ -83,20 +94,20 @@ CREATE TABLE Trabajador(
  );
 
 CREATE TABLE Tarjeta_debito(
-	 numero_tarjeta_debito VARCHAR(20) NOT NULL,
+	 numero_tarjeta_debito VARCHAR(255) NOT NULL,
 	 celular_usuario VARCHAR(10) NOT NULL,
 	 tarjeta_debito_banco VARCHAR(50) NOT NULL,
-	 tarjeta_debito_numero_cuenta VARCHAR(20) NOT NULL,
+	 tarjeta_debito_numero_cuenta VARCHAR(255) NOT NULL,
 	 CONSTRAINT pk_tarjeta_debito PRIMARY KEY (numero_tarjeta_debito),
 	 CONSTRAINT fK_usuario FOREIGN KEY (celular_usuario) REFERENCES Usuario(celular_usuario) ON UPDATE CASCADE ON DELETE RESTRICT
  );
 
  CREATE TABLE Tarjeta_credito(
-	 numero_tarjeta_credito VARCHAR(20) NOT NULL,
+	 numero_tarjeta_credito VARCHAR(255) NOT NULL,
 	 celular_usuario VARCHAR(10) NOT NULL,
 	 tarjeta_credito_banco VARCHAR(50) NOT NULL,
 	 tarjeta_credito_fecha_vencimiento VARCHAR(5) NOT NULL,
-	 tarjeta_credito_cvc VARCHAR(4) NOT NULL,
+	 tarjeta_credito_cvc VARCHAR(255) NOT NULL,
 	 CONSTRAINT pk_tarjeta_credito PRIMARY KEY (numero_tarjeta_credito),
 	 CONSTRAINT fK_usuario FOREIGN KEY (celular_usuario) REFERENCES Usuario(celular_usuario) ON UPDATE CASCADE ON DELETE RESTRICT
  );
@@ -112,8 +123,8 @@ CREATE TABLE Tarjeta_debito(
 	 servicio_hora_fin TIME NOT NULL,
 	 paga_fecha_pago DATE NOT NULL,
 	 paga_valor_pago INT NOT NULL,
-	 numero_tarjeta_debito VARCHAR(20) NOT NULL,
-	 numero_tarjeta_credito VARCHAR(20) NOT NULL,
+	 numero_tarjeta_debito VARCHAR(255) NOT NULL,
+	 numero_tarjeta_credito VARCHAR(255) NOT NULL,
 	 CONSTRAINT pk_servicio PRIMARY KEY (id_servicio),
 	 CONSTRAINT fk_trabajador FOREIGN KEY (cedula_trabajador) REFERENCES Trabajador(cedula_trabajador) ON UPDATE CASCADE ON DELETE RESTRICT,
 	 CONSTRAINT fk_usuario FOREIGN KEY (celular_usuario) REFERENCES Usuario(celular_usuario) ON UPDATE CASCADE ON DELETE RESTRICT,
@@ -126,6 +137,7 @@ CREATE TABLE Tarjeta_debito(
 
  --TRIGGERS
 
+-- Funcion para convertir las coordenadas en puntos para usar con postgis
 CREATE FUNCTION add_geopint() RETURNS TRIGGER AS $$
 DECLARE
 BEGIN
@@ -137,6 +149,87 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trigger_add_geopint BEFORE INSERT ON Direccion
 FOR EACH ROW
 EXECUTE PROCEDURE add_geopint();
+
+-- Funcion para encriptar contrasenias de trabajador
+
+CREATE FUNCTION encrypt_Wpass() RETURNS TRIGGER AS $$
+DECLARE
+BEGIN
+	NEW.trabajador_contrasenia :=
+PGP_SYM_ENCRYPT(NEW.trabajador_contrasenia, 'AES_KEY');
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_encrypt_Wpass BEFORE INSERT OR UPDATE ON Trabajador
+FOR EACH ROW
+EXECUTE PROCEDURE encrypt_Wpass();
+
+-- Funcion para encriptar contrasenias de usuario
+
+CREATE FUNCTION encrypt_Upass() RETURNS TRIGGER AS $$
+DECLARE
+BEGIN
+	NEW.usuario_contrasenia :=
+PGP_SYM_ENCRYPT(NEW.usuario_contrasenia, 'AES_KEY');
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_encrypt_Upass BEFORE INSERT OR UPDATE ON Usuario
+FOR EACH ROW
+EXECUTE PROCEDURE encrypt_Upass();
+
+-- Funcion para encriptar cuenta bancaria
+
+CREATE FUNCTION encrypt_bankAccount() RETURNS TRIGGER AS $$
+DECLARE
+BEGIN
+	NEW.numero_cuenta_bancaria :=
+PGP_SYM_ENCRYPT(NEW.numero_cuenta_bancaria, 'AES_KEY');
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_encrypt_bankAccount BEFORE INSERT ON Cuenta_bancaria
+FOR EACH ROW
+EXECUTE PROCEDURE encrypt_bankAccount();
+
+-- Funcion para encriptar Tarjeta debito
+
+CREATE FUNCTION encrypt_Tdebit() RETURNS TRIGGER AS $$
+DECLARE
+BEGIN
+	NEW.numero_tarjeta_debito :=
+PGP_SYM_ENCRYPT(NEW.numero_tarjeta_debito, 'AES_KEY');
+
+	NEW.tarjeta_debito_numero_cuenta :=
+PGP_SYM_ENCRYPT(NEW.tarjeta_debito_numero_cuenta, 'AES_KEY');
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_encrypt_Tdebit BEFORE INSERT ON Tarjeta_debito
+FOR EACH ROW
+EXECUTE PROCEDURE encrypt_Tdebit();
+
+-- Funcion para encriptar Tarjeta credito
+
+CREATE FUNCTION encrypt_Tcredit() RETURNS TRIGGER AS $$
+DECLARE
+BEGIN
+	NEW.numero_tarjeta_credito :=
+PGP_SYM_ENCRYPT(NEW.numero_tarjeta_credito, 'AES_KEY');
+
+	NEW.tarjeta_credito_cvc :=
+PGP_SYM_ENCRYPT(NEW.tarjeta_credito_cvc, 'AES_KEY');
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_encrypt_Tcredit BEFORE INSERT ON Tarjeta_credito
+FOR EACH ROW
+EXECUTE PROCEDURE encrypt_Tcredit();
 
 
 INSERT INTO Labor(labor_nombre) VALUES('Profesor Ingles'),
